842c3925cb8367303b062677c975cb63732310ee
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-07 16:44:51 +0300

    task 1

---
+++ dataTypes/dogYears.js
@@ -0,0 +1,24 @@
+let input = [
+    "4",
+    
+];
+ 
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let humanYears = Number(gets())
+let humanYearsOver = 0
+let dogYears = 0
+
+if (humanYears > 2){
+    humanYearsOver += humanYears - 2
+}
+
+if (humanYears <= 2){
+    dogYears += humanYears * 10
+} else{
+    dogYears += 2 * 10
+    dogYears += humanYearsOver * 4
+}
+
+console.log(dogYears)
