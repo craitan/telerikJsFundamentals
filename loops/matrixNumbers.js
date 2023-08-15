a61994647a9265e4602a6b201d675838a6b5e57d
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-15 11:55:51 +0300

    task 8 loops

---
+++ loops/matrixNumbers.js
@@ -0,0 +1,14 @@
+let input = ["3"];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let n = Number(gets());
+
+for (let i = 1; i <= n; i++) {
+    let row = [];
+    for (let j = i; j < i + n; j++) {
+      row.push(j);
+    }
+    console.log(row.join(' '));
+  }
