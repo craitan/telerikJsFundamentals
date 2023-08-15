f2bdf4eff320605bfbe01d6ca4352dae457b4e37
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-15 14:37:14 +0300

    task 12 loops

---
+++ loops/primeFactors.js
@@ -0,0 +1,14 @@
+let input = ["12"];
+
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let n = Number(gets())
+let devisor = 2
+while (devisor !== 0) {
+    if (n % devisor !== 0) {
+        devisor++
+    } else if (n % devisor === 0) {
+        n = n / devisor
+        console.log(devisor)
+    }
+}
