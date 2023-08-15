42ddd15827f96a49f86ba73743a6961828935307
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-15 12:39:25 +0300

    task 10 loops

--- loops/findMaximumValue.js
+++ loops/findMaximumValue.js
@@ -3,12 +3,7 @@ let input = ["3", "1", "2", "3"];
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
 let n = Number(gets());
-let for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        smallestNumber
-    }
-} = -200;
+smallestNumber = -200;
 for (let i = 1; i <= n; i++) {
     let currentNumber = Number(gets());
     if (currentNumber > smallestNumber){
