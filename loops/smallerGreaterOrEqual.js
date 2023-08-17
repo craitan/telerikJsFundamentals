64c4142b855f88cd38a6f487e6e71912c959aa9b
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-17 12:00:32 +0300

    task 4 loops homework update

--- loops/smallerGreaterOrEqual.js
+++ loops/smallerGreaterOrEqual.js
@@ -3,18 +3,16 @@ let input = ["3", "2", "5", "1"];
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
 let n = Number(gets());
+let previusNumber = 0;
 let comparisonString = '';
 
-for (let i = 1; i < n; i++) {
-    if (input[i] < input[i + 1]) {
-      comparisonString += input[i] + "<";
-    } else if (input[i] > input[i + 1]) {
-      comparisonString += input[i] + ">";
-    } else {
-      comparisonString += input[i] + "=";
-    }
+for (let i = 1; i <= n; i++) {
+  let currentNumber = Numbver(gets());
+
+  if (comparisonString.length < 1) {
+    comparisonString += currentNumber.toString();
   }
-  
-  comparisonString += input[n - 1];
-  
-  console.log(comparisonString);
+
+
+}
+console.log(comparisonString)
---
+++ loops/wordOrNumber.js
@@ -0,0 +1,15 @@
+let input = ["asd"];
+
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+
+let commnad = gets();
+
+if (!isNaN(commnad)) {
+    let number = parseFloat(commnad);
+    let result = number + 1
+    console.log(result)
+}else{
+    let reversedWord = commnad.split('').reverse().join("")
+    console.log(reversedWord)
+}
