80f8b60d1cae49f0b49f01125954c171136b2c4f
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-08-28 17:37:27 +0300

    task 6 exam prep

---
+++ exams/aloneNumbers.js
@@ -0,0 +1,24 @@
+let input = ["1, 2, 3, 2, 5, 2", "2"];
+//let input = ["1, 2, 3", "2"];
+
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let numbers = gets().split(', ').map(Number);
+
+let aloneNumber = Number(gets());
+
+for (let i = 1; i < numbers.length - 1; i++) {
+    if ((numbers[i] === aloneNumber) &&
+        (numbers[i] !== numbers[i - 1]) &&
+        (numbers[i] !== numbers[i + 1])) {
+
+        if (numbers[i - 1] > numbers[i + 1]) {
+            numbers[i] = numbers[i - 1];
+        } else {
+            numbers[i] = numbers[i + 1];
+        }
+    }
+}
+
+
+console.log(numbers);
