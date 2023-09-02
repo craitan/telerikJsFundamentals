b70f1df98424213131c92e3c5d1ea2fb9cc80eff
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-09-02 14:31:53 +0300

    task 8 exam prep

--- exams/greaterNumber.js
+++ exams/greaterNumber.js
@@ -1,5 +1,5 @@
-//let input = ["4,1,2", "1,3,4,2"];
-let input = ["2,4", "1,2,3,4"];
+let input = ["4,1,2", "1,3,4,2"];
+//let input = ["2,4", "1,2,3,4"];
 
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
@@ -11,25 +11,23 @@ let array = []
 for (let i = 0; i < firstArray.length; i++) {
     let currentNumber = firstArray[i];
 
-    // for (let j = 0; j < secondArray.length - 1; j++) {
-    //     if (secondArray[j] === currentNumber) {
-    //         secondArray[j + 1]
-    //     }
-    // }
+   
     let indexInSecondArray = secondArray.indexOf(currentNumber);
 
     let newArray = secondArray.slice(indexInSecondArray + 1)
 
-    if (indexInSecondArray === secondArray.length - 1) {
-        array.push(-1);
-    }
+    let foundGreater = false;
+
     for (num of newArray) {
         if (num > currentNumber) {
             array.push(num);
+            foundGreater = true
             break;
-        } else {
-            array.push(-1);
         }
+    }
+
+    if(!foundGreater){
+        array.push(-1)
     }
 }
 
