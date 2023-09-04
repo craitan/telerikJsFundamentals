fd7a4f319e3eea41b650c60508558b8ef53431bc
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-09-04 18:25:04 +0300

    task 12 exam prep

---
+++ exams/shootingContest.js
@@ -0,0 +1,25 @@
+let input = ["4", "5", "1", "3", "5"];
+//let input = ["2,4", "1,2,3,4"];
+
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+
+let targets = Number(gets());
+
+let georgiSpeed = Number(gets());
+let georgiLatency = Number(gets());
+
+let petarSpeed = Number(gets());
+let petarLatency = Number(gets());
+
+let georgiScore = (targets * georgiSpeed) + (georgiLatency * 2);
+
+let petarScore = (targets * petarSpeed) + (petarLatency * 2);
+
+if (georgiScore < petarScore) {
+    console.log("George");
+} else if (georgiScore > petarScore) {
+    console.log("Peter");
+} else if (georgiScore === petarScore) {
+    console.log('Draw')
+}
